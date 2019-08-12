db8d5372f6e06ce4d4a5dd773f2f82ef5b71b1b2
Katerina Zenchik <e.zenchik@gmail.com>
2019-08-12 13:22:01 +0300

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { simpleAction } from './actions/simpleAction';
-
 import logo from './logo.svg';
 import './App.css';
 
-const mapStateToProps = state => ({
-  ...state
-});
-
-const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction())
-});
-
-
-class App extends React.Component {
-  simpleAction = (event) => {
-    this.props.simpleAction();
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+and save to reload.
         </p>
-        <pre>
- {
-   JSON.stringify(this.props)
- }
-</pre>
-        <button onClick={this.simpleAction}>Test redux action</button>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
