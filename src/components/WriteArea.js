980bbe409e915febb63388b32194e63f70002780
Katerina Zenchik <e.zenchik@gmail.com>
2019-08-14 23:33:26 +0300

    style/fix: write area style

--- src/components/WriteArea.js
+++ src/components/WriteArea.js
@@ -10,8 +10,10 @@ export default function WriteArea(props) {
 
   return (
     <div className="write_area">
+      <div className="write_area__wrap">
       <textarea ref={myMsg} className="write_area__text" />
       <button type="button" className="write_area__send" onClick={send}>Send</button>
+      </div>
     </div>
   );
 }
