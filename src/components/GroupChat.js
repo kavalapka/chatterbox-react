97a2e3cd9cbddea84eabc660062d3d3867943fb9
Katerina Zenchik <e.zenchik@gmail.com>
2019-08-15 21:29:02 +0300

    style/fix: fix showing messages

--- src/components/GroupChat.js
+++ src/components/GroupChat.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Message from './Message';
 import './GroupChat.css';
 
 export default function GroupChat(props) {
   const { messages } = props;
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    // console.log('SCROLL: ', scrollRef.current.scrollTop = scrollRef.current.scrollHeight);
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  });
   return (
-    <div className="group_chat">
+    <div className="group_chat" ref={scrollRef}>
       { messages.map((msg, idx) => <Message key={msg.id} idx={idx} message={msg} />) }
     </div>
   );
